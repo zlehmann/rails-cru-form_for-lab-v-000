11477a629a04340d45592b66ddd09972611a95ec
Zachary Lehmann <zlehmann@gmail.com>
2019-01-18 01:57:53 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,4 +9,16 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist = Artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 end
