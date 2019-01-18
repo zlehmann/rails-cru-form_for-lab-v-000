47a4e5fe83f92c29beda05c89a4aaa1fc89d2ca6
Zachary Lehmann <zlehmann@gmail.com>
2019-01-18 01:48:10 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,15 @@
 class GenresController < ApplicationController
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
 end
