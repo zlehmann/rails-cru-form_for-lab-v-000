54d32df234c39dd46c715f34b3088a61ef1a211f
Zachary Lehmann <zlehmann@gmail.com>
2019-01-18 01:54:27 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -15,6 +15,10 @@ class GenresController < ApplicationController
 
   def edit
     @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
     @genre.update(params.require(:genre).permit(:name))
     redirect_to genre_path(@genre)
   end
